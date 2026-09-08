4169927b654b53526821b835a5358530e336376e
Yeusepe <subunit_swings0h@icloud.com>
2026-09-08 18:19:50 -0500

    chore(release): bump version to 0.1.12

--- universal_spp_plugin/lib/updater.py
+++ universal_spp_plugin/lib/updater.py
@@ -15,7 +15,7 @@ import urllib.request
 import zipfile
 
 
-PLUGIN_VERSION = "0.1.11"
+PLUGIN_VERSION = "0.1.12"
 OWNER = "Yeusepe"
 REPO = "Universal-Painter-Files"
 RELEASES_API_URL = f"https://api.github.com/repos/{OWNER}/{REPO}/releases"
